35fba3f424b6de50bfea25e04831a2e7d6efff44
Christian Santanas <50605403+chris171290@users.noreply.github.com>
2024-10-08 15:13:07 -0400

    feat: :sparkles: add book ride and stripe provider

---
+++ components/Payment.tsx
@@ -0,0 +1,109 @@
+import CustomButton from "@/components/CustomButton";
+import { PaymentSheetError, useStripe } from "@stripe/stripe-react-native";
+import { Alert } from "react-native";
+import { useEffect, useState } from "react";
+import { fetchAPI } from "@/lib/fetch";
+import { PaymentProps } from "@/types/type";
+
+const Payment = ({
+  fullName,
+  email,
+  amount,
+  driverId,
+  rideTime,
+}: PaymentProps) => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [success, setSuccess] = useState(false);
+
+  const confirmHandler = async (paymentMethod, _, intentCreationCallback) => {
+    // Make a request to your own server.
+    const { paymentIntent, customer } = await fetchAPI(
+      "/(api)/(stripe)/create",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: fullName || email.split("@")[0],
+          email: email,
+          amount: amount,
+          paymentMethodId: paymentMethod.id,
+        }),
+        // eslint-disable-next-line prettier/prettier
+      }
+    );
+
+    if (paymentIntent.client_secret) {
+      const { result } = await fetchAPI("/(api)/(stripe)/pay", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          payment_method_id: paymentMethod.id,
+          payment_intent_id: paymentIntent.id,
+          customer_id: customer,
+          client_secret: paymentIntent.client_secret,
+        }),
+      });
+
+      if (result.client_secret) {
+        // rides/create
+      }
+    }
+    // Call the `intentCreationCallback` with your server response's client secret or error
+    const { client_secret, error } = await response.json();
+    if (client_secret) {
+      intentCreationCallback({ clientSecret: client_secret });
+    } else {
+      intentCreationCallback({ error });
+    }
+  };
+
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: 1099,
+          currencyCode: "USD",
+        },
+        confirmHandler: confirmHandler,
+      },
+    });
+    if (error) {
+      // handle error
+    }
+  };
+
+  useEffect(() => {
+    initializePaymentSheet();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      if (error.code === PaymentSheetError.Canceled) {
+        Alert.alert(`Error code: ${error.code}`, error.message);
+      } else {
+        setSuccess(true);
+      }
+    }
+  };
+
+  return (
+    <>
+      <CustomButton
+        title="Confirm Ride"
+        className="my-10"
+        onPress={openPaymentSheet}
+      />
+    </>
+  );
+};
+
+export default Payment;
